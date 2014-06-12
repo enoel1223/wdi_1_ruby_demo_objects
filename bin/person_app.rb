d3564ce2c9626d8c14878400a23206de945704c9
Eddie Noel <enoel1223@gmail.com>
2014-06-12 15:55:48 -0400

    applied a person and song

--- bin/person_app.rb
+++ bin/person_app.rb
@@ -1,2 +1,34 @@
 # This will "insert" the contents of the person.rb here
+
 require_relative '../lib/person.rb'
+
+
+tom = Person.new("Tom", "Dyer", '06-02-1997')
+jill = Person.new("Jill", "Stein", '02-01-1992')
+
+
+puts " tom is #{tom.first_name}"
+puts "jill is #{jill.last_name}"
+
+#using the setter method to change toms last name
+#tom.last_name = "eddie"
+#tom.last_name = "Jones"
+
+puts "tom's last name is #{tom.last_name}"
+puts "tom's first name is #{tom.first_name}"
+
+puts "tom's full name is #{tom.full_name}"
+
+
+
+puts "#{tom.age}"
+
+tom.current_smoker = false
+tom.married_status = false
+#what is the value of give insurance, should or should not
+msg = tom.give_insurance? ? "should" : "should not"
+msg_marry = tom.married? ? "He should get a girlfriend" : "He should stay single"
+puts "Tom #{msg} get insured"
+puts "Tom #{msg_marry}"
+
+
--- bin/song_app.rb
+++ bin/song_app.rb
@@ -4,3 +4,27 @@ require_relative '../lib/song.rb'
 
 # Add code to use the Song class here.
 
+song1 = Song.new("Magic", "Coldplay", 342, 129)
+song2 = Song.new("Walk on", "U2", 302, 129)
+song3 = Song.new("Fix you", "Coldplay", 412, 129)
+
+
+
+puts song1.inspect
+puts song2.inspect
+puts song3.inspect
+
+
+average_songs = (song1.duration + song2.duration + song3.duration) / 3
+
+
+puts average_songs
+
+
+puts song1.duration = 3223
+
+puts average_songs = (song1.duration + song2.duration + song3.duration) / 3
+
+
+
+puts song1.title = "yellow"
